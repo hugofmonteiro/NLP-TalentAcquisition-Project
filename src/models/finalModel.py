1d0c9320cf760f8180cf8b2e8f8ee33361136ff8
Hugo F. Monteiro <hmonteiro@ucdavis.edu>
2023-11-26 23:41:10 -0800

    updated cosine calculation and reranking

--- src/models/finalModel.py
+++ src/models/finalModel.py
@@ -3,9 +3,6 @@ from transformers import BertTokenizer, BertModel
 from scipy.spatial.distance import cosine
 import pandas as pd
 
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-
 df = pd.read_csv('../data/potential-talents - Aspiring human resources - seeking human resources.csv')
 
 # Load pre-trained model tokenizer and model
@@ -19,46 +16,54 @@ def bert_encode(text):
         output = model(**encoded_input)
     return output.last_hidden_state.mean(dim=1).squeeze()
 
-# Function to calculate cosine similarity
 def cosine_similarity(vec1, vec2):
-    return 1 - cosine(vec1, vec2)
+    vec1 = vec1.view(-1)
+    vec2 = vec2.view(-1)
+    dot_product = torch.dot(vec1, vec2)
+    norm_vec1 = torch.norm(vec1)
+    norm_vec2 = torch.norm(vec2)
+    return dot_product / (norm_vec1 * norm_vec2)
 
-# Define key phrases for first FIT assessment
+# Defining key phrases for first FIT assessment
 key_phrases = ["aspiring human resources", "seeking human resources"]
 
-# Compute BERT embeddings for key phrases
+# Computing BERT embeddings for key phrases
 key_phrase_embeddings = [bert_encode(phrase) for phrase in key_phrases]
 
-# Calculate similarity for job titles
+# Calculating similarity for job titles
 df['tokenized_titles'] = df['job_title'].apply(lambda x: x.lower())
 for idx, phrase in enumerate(key_phrases):
     phrase_embedding = key_phrase_embeddings[idx]
     df['similarity_to_' + phrase.replace(" ", "_")] = df['tokenized_titles'].apply(
-        lambda x: cosine_similarity(bert_encode(x), phrase_embedding)
+        lambda x: cosine_similarity(bert_encode(x), phrase_embedding).item()
     )
 
-# Calculate similarity for 'location', so chose a city based on stared candidate
-df['tokenized_locations'] = df['location'].apply(lambda x: x.lower())
-chicago_embedding = bert_encode("Chicago, Illinois")
-df['similarity_to_Chicago_Illinois'] = df['tokenized_locations'].apply(
-    lambda x: cosine_similarity(bert_encode(x), chicago_embedding)
-)
+# Calculating average of similarities
+df['AVG_cosine_similarity'] = (df['similarity_to_aspiring_human_resources'] + df['similarity_to_seeking_human_resources'])/2
 
-def connection_to_prob(connection):
-    if connection.strip() == '500+':
-        return 1.00
-    else:
-        # Remove any non-numeric characters before conversion
-        numeric_connection = ''.join(filter(str.isdigit, connection))
-        return min(int(numeric_connection) / 500, 1.00)
-
-df['connection_prob'] = df['connection'].apply(connection_to_prob)
-
-# Calculate probabilities based on FIT, FIT plus Connections, or even FIT plus Connections adjusted to similar city to stared candidated
-df['FIT'] = df['similarity_to_aspiring_human_resources'] + df['similarity_to_seeking_human_resources']
-df['FIT_AND_CONNECTIONS'] = df['similarity_to_aspiring_human_resources'] + df['similarity_to_seeking_human_resources'] + df['similarity_to_Chicago_Illinois']
-df['ALL_Ajusted_Location'] = df['FIT_AND_CONNECTIONS'] + df['connection_prob']
-
-# Candidates can be ranked based on FIT, FIT plus Connections, or even FIT plus Connections adjusted to similar city to stared candidated
-df_ranked = df.sort_values(by='FIT', ascending=False)
+# Ranking candidates based on most similar to keywords
+df_ranked = df.sort_values(by='AVG_cosine_similarity', ascending=False)
 df_ranked
+
+# Reranking based on job_title of stared candidate
+starred_candidate_job_title = "Human Resources Coordinator"
+
+# Calculating BERT embedding
+starred_job_title_embedding = bert_encode(starred_candidate_job_title)
+
+# Modifying key phrase embeddings based on the starred candidate
+new_key_phrase_embeddings = [(bert_encode(phrase) + starred_job_title_embedding) / 2 for phrase in key_phrases]  # Average method
+
+# Recalculating the similarities with new embeddings
+for idx, phrase in enumerate(key_phrases):
+    new_phrase_embedding = new_key_phrase_embeddings[idx]
+    df['new_similarity_to_' + phrase.replace(" ", "_")] = df['tokenized_titles'].apply(
+        lambda x: cosine_similarity(bert_encode(x), new_phrase_embedding).item()
+    )
+
+# Update the average fit score
+df['AVG_cosine_similarity_reranked'] = df[['new_similarity_to_' + phrase.replace(" ", "_") for phrase in key_phrases]].mean(axis=1)
+
+# Rerank the candidates based on the new fit score
+df_reranked = df.sort_values(by='AVG_cosine_similarity_reranked', ascending=False)
+df_reranked
